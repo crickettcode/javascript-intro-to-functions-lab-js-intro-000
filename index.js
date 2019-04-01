9d148c9012cff0d9e6de19f69e2d9647535d466d
Crickett Sykes <crickettcode@gmail.com>
2019-04-01 01:16:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
   return 'I love you, too.'
 } else if
-  (string.toLowerCase === .toLowerCase()) {
+  (string.toLowerCase === toLowerCase()) {
   return "I can't hear you!"
 }
 }
